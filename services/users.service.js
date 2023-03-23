03a6d0d5aae8be718fa70da517ee71948d19d9be
tienthinh-fe <thinhhtde150233@fpt.edu.vn>
2023-03-23 14:06:00 +0700

    Merge main into user-api

--- services/users.service.js
+++ services/users.service.js
@@ -2,6 +2,8 @@
 
 const { MoleculerClientError } = require('moleculer').Errors
 
+const { sha256 } = require('js-sha256')
+const otpGenerator = require('otp-generator')
 const jwt = require('jsonwebtoken')
 
 const DbService = require('../mixins/db.mixin')
@@ -20,7 +22,14 @@ module.exports = {
 
 		JWT_SECRET: process.env.JWT_SECRET || 'jwt-conduit-secret',
 
-		fields: ['_id', 'username', 'email', 'handler', 'token'],
+		fields: [
+			'_id',
+			'username',
+			'email',
+			'handler',
+			'accessToken',
+			'refreshToken',
+		],
 
 		entityValidator: {
 			username: { type: 'string', min: 2, max: 15 },
@@ -90,52 +99,134 @@ module.exports = {
 		},
 
 		/**
+		 * Login OTP with email
+		 *
+		 * @actions
+		 * @param {String} email - User's email address
+		 *
+		 * @returns {Object} full hash - combined hash of email, code and expiration time
+		 *
+		 */
+		login: {
+			rest: 'POST /login',
+			params: {
+				email: { type: 'email' },
+			},
+			async handler(ctx) {
+				const otp = this.generateOtp()
+				const ttl = 5 * 60 * 1000 // 5 minutes
+				const expires = Date.now() + ttl // 5 minutes from now
+				const data = `${ctx.params.email}.${otp}.${expires}`
+				const hash = sha256(data)
+				const fullHash = `${hash}.${expires}`
+
+				await ctx.call('email.send', {
+					to: ctx.params.email,
+					otp,
+				})
+
+				return {
+					fullHash,
+				}
+			},
+		},
+
+		/**
 		 * Verify a user's email address.
 		 *
 		 * @actions
 		 * @param {String} email - User's email address
 		 * @param {String} code - Verification code
+		 * @param {String} hash - Combined hash of email, code and expiration time
 		 *
-		 * @returns {Object} User entity
+		 * @returns {Object} token - JWT token
 		 */
 		verifyOtp: {
 			rest: 'POST /verify',
 			params: {
-				code: { type: 'string' },
+				otp: { type: 'string' },
 				email: { type: 'string' },
+				hash: { type: 'string' },
 			},
 			async handler(ctx) {
-				const { code, email } = ctx.params
+				const { otp, hash, email } = ctx.params
+				const currentDate = new Date()
 
-				const isVerified = await this.verifyOtp(email, code)
+				let [hashValue, expirationTime] = hash.split('.')
 
-				if (isVerified) {
-					// if email of user has in database, return user
-					// else create new user
-					const user = await this.adapter.findOne({ email })
-
-					if (user) {
-						return user
-					} else {
-						const entity = {
-							username: email,
-							email: email,
-						}
-
-						const json = await this.createNewUser(
-							entity.email,
-							entity.username,
-							ctx
-						)
-						return json
-					}
-				} else {
-					throw new MoleculerClientError('Invalid code', 422, '', [
+				if (!otp) {
+					throw new MoleculerClientError('Invalid otp', 422, '', [
 						{
-							field: 'code',
-							message: 'is invalid',
+							field: 'otp',
+							message: 'is required',
 						},
 					])
+				}
+
+				if (!hash) {
+					throw new MoleculerClientError('Invalid hash', 422, '', [
+						{
+							field: 'hash',
+							message: 'is required',
+						},
+					])
+				}
+
+				if (!email) {
+					throw new MoleculerClientError('Invalid email', 422, '', [
+						{
+							field: 'email',
+							message: 'is required',
+						},
+					])
+				}
+
+				if (currentDate.getTime() > parseInt(expirationTime)) {
+					throw new MoleculerClientError('Expired otp', 422, '', [
+						{
+							field: 'otp',
+							message: 'is expired',
+						},
+					])
+				} else {
+					const data = `${email}.${otp}.${expirationTime}`
+					const calculatedHash = sha256(data)
+
+					if (calculatedHash === hashValue) {
+						const { accessToken, refreshToken } = await this.generateJWT(email)
+
+						let user = await this.adapter.findOne({
+							email,
+						})
+
+						if (!user) {
+							user = await this.createNewUser(
+								email,
+								null,
+								accessToken,
+								refreshToken,
+								ctx
+							)
+						} else {
+							user = await this.adapter.updateById(user._id, {
+								accessToken,
+								refreshToken,
+							})
+						}
+
+						return {
+							accessToken,
+							refreshToken,
+							user,
+						}
+					} else {
+						throw new MoleculerClientError('Invalid otp', 422, '', [
+							{
+								field: 'otp',
+								message: 'is invalid',
+							},
+						])
+					}
 				}
 			},
 		},
@@ -159,71 +250,66 @@ module.exports = {
 
 	methods: {
 		/**
-		 * Generate a JWT token from user entity
-		 *
-		 * @param {Object} user
-		 */
-		generateJWT(user) {
-			const today = new Date()
-			const exp = new Date(today)
-			exp.setDate(today.getDate() + 60)
-
-			return jwt.sign(
-				{
-					id: user._id,
-					username: user.username,
-					exp: Math.floor(exp.getTime() / 1000),
-				},
-				this.settings.JWT_SECRET
-			)
-		},
-
-		/**
-		 * Transform returned user entity. Generate JWT token if neccessary.
-		 *
-		 * @param {Object} user
-		 * @param {Boolean} withToken
-		 */
-		transformEntity(user, withToken, token) {
-			if (user) {
-				if (withToken) user.token = token || this.generateJWT(user)
-			}
-
-			return { user }
-		},
-
-		/**
-		 * Verify code from email
+		 * Generate a JWT token from email
 		 *
 		 * @param {String} email
-		 * @param {String} code
 		 */
-		async verifyOtp(email, code) {
-			// temporary
-			const emailAndCodeMapping = {
-				'sonha@gmail.com': '123456',
-				'tienthinh@gmail.com': '654321',
-			}
+		generateJWT(email) {
+			const payload = { email }
 
-			if (emailAndCodeMapping[email] === code) {
-				return true
-			} else {
-				return false
-			}
+			const accessToken = jwt.sign(payload, this.settings.JWT_SECRET, {
+				expiresIn: '30m',
+			})
+
+			const refreshToken = jwt.sign(payload, this.settings.JWT_SECRET, {
+				expiresIn: '7d',
+			})
+
+			return { accessToken, refreshToken }
 		},
 
-		async createNewUser(email, username, ctx) {
+		/**
+		 * Generate a random OTP
+		 *
+		 * @returns {String} otp
+		 *
+		 * @example
+		 * 123456
+		 */
+		generateOtp() {
+			return otpGenerator.generate(6, {
+				digits: true,
+				lowerCaseAlphabets: false,
+				upperCaseAlphabets: false,
+				specialChars: false,
+			})
+		},
+
+		/**
+		 * Create a new user
+		 *
+		 * @param {String} email
+		 * @param {String} username
+		 * @param {String} accessToken
+		 * @param {String} refreshToken
+		 * @param {Object} ctx
+		 *
+		 * @returns {Object} user
+		 *
+		 */
+		async createNewUser(email, username, accessToken, refreshToken, ctx) {
 			const entity = {
 				username: username,
 				email: email,
 				handler: username + '#' + Math.floor(1000 + Math.random() * 9000),
+				accessToken: accessToken,
+				refreshToken: refreshToken,
 			}
 
 			const doc = await this.adapter.insert(entity)
 			const user = await this.transformDocuments(ctx, {}, doc)
-			const json = await this.transformEntity(user, true)
-			await this.entityChanged('created', json, ctx)
-			return json
+			await this.entityChanged('created', user, ctx)
+			return user
 		},
 	},
 }
